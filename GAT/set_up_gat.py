f8df748f90b86e12090e647689e27c4c7892d229
Niccolò Biondi <niccolo.biondi1@stud.unifi.it>
2020-03-31 09:06:48 +0200

    custom early stopping for both acc and loss

--- GAT/set_up_gat.py
+++ GAT/set_up_gat.py
@@ -11,16 +11,74 @@ from utils import read_dataset, permute, split, one_hot_enc
 from GAT.train import train, test
 from GAT.layers import GAT
 
-def set_up_gat(dataset, epochs, batch_size, val_period, log, seed):
+class RegularizedLoss(tf.keras.losses.Loss):
+
+    def __init__(self, l2_weight):
+        super().__init__()
+        self.l2_weight = l2_weight
+
+    def call(self, y_true, y_pred):
+        loss = tf.keras.backend.categorical_crossentropy(y_true, y_pred)
+        l2_loss = tf.nn.l2_loss(y_pred-y_true)
+        return loss + self.l2_weight * l2_loss
+
+
+class EarlyStop(tf.keras.callbacks.Callback):
+
+    def __init__(self, model, monitor, patience=0, save_model=False, checkpoint_path='./'):
+        super(EarlyStop, self).__init__()
+        self.model = model
+        self.monitor = monitor
+        self.modality = 'acc' if 'acc' in monitor.name else 'loss'
+        self.patience = patience
+        self.best_weights = None
+        self.save_model = save_model
+        if self.save_model:
+            ckpt_name = self.model.name + 'ckpts/cp-{epoch:03d}-' + self.modality + '-{v:03f}.ckpt'
+            self.checkpoint_path = os.path.join(checkpoint_path, ckpt_name)
+
+    def on_train_begin(self):
+        # The number of epoch it has waited when loss is no longer minimum.
+        self.wait = 0
+        # The epoch the training stops at.
+        self.stopped_epoch = 0
+        # Initialize the best as infinity.
+        self.best = np.Inf if self.modality == 'loss' else 0
+
+    def on_epoch_end(self, epoch):
+        current = self.monitor.result()
+        if (np.less(current, self.best) and self.modality == 'acc') or (np.greater(current, self.best) and self.modality == 'loss'):
+            self.best = current
+            self.wait = 0
+            # Record the best weights if current results is better (less).
+            self.best_weights = self.model.get_weights()
+            if self.save_model:
+                self.model.save_weights(self.checkpoint_path.format(
+                    epoch=epoch, v=self.best))
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.model.stop_training = True
+                print('Restoring model weights from the end of the best epoch.')
+                self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0:
+            print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+
+
+def set_up_gat(dataset, epochs, l2_weight, val_period, log, seed):
 
     print("GAT!")
 
     # Preprocess on data
     features, neighbors, labels, o_h_labels, keys = read_dataset(dataset)
-    features, neighbors, labels, o_h_labels, keys = permute(features, neighbors, labels, o_h_labels, keys, seed)
+    features, neighbors, labels, o_h_labels, keys = permute(
+        features, neighbors, labels, o_h_labels, keys, seed)
     train_idx, val_idx, test_idx = split(dataset, labels)
 
-    loss_fn = tf.keras.losses.CategoricalCrossentropy()
+    loss_fn = RegularizedLoss(l2_weight)
 
     train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
     train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_acc')
@@ -29,13 +87,9 @@ def set_up_gat(dataset, epochs, batch_size, val_period, log, seed):
 
     if dataset == 'pubmed':
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
-    else: 
+    else:
         optimizer = tf.keras.optimizers.Adam(learning_rate=5e-3)
-    
-    sched_loss = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=100)
-    sched_acc = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=100)
-    # tensorboard = tf.keras.callbacks.Tensorboard(log_dir='.logs/')
-
+        
     if dataset == 'pubmed':
         n_output_heads = 8
     else:
@@ -43,9 +97,12 @@ def set_up_gat(dataset, epochs, batch_size, val_period, log, seed):
 
     model = GAT(neighbors, len(o_h_labels[0]), n_output_heads=n_output_heads)
 
-    train(model, features, o_h_labels, features[val_idx], o_h_labels[val_idx],
-        epochs, optimizer, loss_fn, train_loss, train_accuracy, val_loss, val_acc, val_period)
-    
+    sched_acc = EarlyStop(model, monitor=val_acc, patience=100)
+    sched_loss = EarlyStop(model, monitor=val_loss, patience=100)
+
+    train(model, features, o_h_labels, train_idx, val_idx, epochs, optimizer, loss_fn,
+          train_loss, train_accuracy, val_loss, val_acc, [sched_acc, sched_loss], val_period)
+
     test()
 
 
@@ -54,10 +111,10 @@ if __name__ == '__main__':
     dataset = "cora"    # "cora" "pubmed" "citeseer"
 
     epochs = 10
-    val_period = 50        # each epoch validation
+    val_period = 1        # each epoch validation
     log = 1                 # every two epochs print train loss and acc
-    
-    batch_size = 32
-    seed = 1234   
 
-    set_up_gat(dataset, epochs, batch_size, val_period, log, seed)
+    l2_weight = 5e-4
+    seed = 1234
+
+    set_up_gat(dataset, epochs, l2_weight, val_period, log, seed)
--- GAT/train.py
+++ GAT/train.py
@@ -1,35 +1,46 @@
 import tensorflow as tf
 import numpy as np
 
+from tqdm import tqdm
 
-def train(model, x_train, y_train, x_val, y_val, epochs, optimizer,
-        loss_fn, train_loss, train_acc, val_loss, val_acc, val_period):
 
-    for epoch in range(epochs+1):
+def train(model, features, labels, train_mask, val_mask, epochs, optimizer, loss_fn,
+        train_loss, train_acc, val_loss, val_acc, callbacks, val_period):
+
+    for c in callbacks:
+        c.on_train_begin()
+
+    for epoch in tqdm(range(1, epochs+1)):
         
         print("Epoch: {:d} ==> ".format(epoch), end=' ')
 
         with tf.GradientTape() as tape:
-            predictions = model(x_train)
-            loss = loss_fn(y_train, predictions)
+            predictions = model(features)
+            loss = loss_fn(labels[train_mask], predictions[train_mask])
         gradients = tape.gradient(loss, model.trainable_weights)
         optimizer.apply_gradients(zip(gradients, model.trainable_weights))
 
         train_loss(loss)
-        train_acc(y_train, predictions)
+        train_acc(labels[train_mask], predictions[train_mask])
 
-        print("Train Loss: {:.3f}, Train Accuracy: {:.2f}%".format(train_loss.result(), train_acc.result()*100))
+        tqdm.write("\repoch: {}/{}, loss: {:.4f}, accuracy: {:.4f}".format(epoch, epochs, train_loss.result(), train_acc.result()))
 
-        """ if epoch % val_period == 0:
+        if epoch % val_period == 0:
 
-            predictions = model(x_val)
-            loss = loss_fn(y_val, predictions)
+            predictions = model(features)
+            loss = loss_fn(labels[val_mask], predictions[val_mask])
 
             val_loss(loss)
-            val_acc(y_val, predictions)
+            val_acc(labels[val_mask], predictions[val_mask])
 
-            print("\nEpoch {:d}, Validation Loss: {:.3f}, Validation Accuracy: {:.2f}%\n".format(epoch, val_loss.result(), val_acc.result()*100)) """
- 
+            print("Validation loss: {:.4f}, accuracy: {:.4f}\n".format(epoch, val_loss.result(), val_acc.result()))
+        
+        for c in callbacks:
+            c.on_epoch_end(epoch)
+
+    for c in callbacks:
+        c.on_train_end()
+
     return
 
 
