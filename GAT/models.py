a36d27bd90ac908bd6a3548e00923d648ef6a12d
Niccolò Biondi <niccolo.biondi1@stud.unifi.it>
2020-04-07 10:07:50 +0200

    fix gat drop in val

--- GAT/models.py
+++ GAT/models.py
@@ -8,22 +8,22 @@ class GAT(tf.keras.Model):
     def __init__(self, graph, nlabels, nhidden, nheads, feat_drop_rate, coefs_drop_rate):
         super().__init__()
         
-        self.atts = [Attention_layer(graph, nhidden, 'elu', heads, coefs_drop_rate, reduction=False)
+        self.atts = [Attention_layer(graph, nhidden, 'elu', heads, feat_drop_rate, coefs_drop_rate, reduction=False)
                     for heads in nheads[:-1]]
-        self.att_out = Attention_layer(graph, nlabels, 'softmax', nheads[-1], coefs_drop_rate, reduction=True)
+        self.att_out = Attention_layer(graph, nlabels, 'softmax', nheads[-1], feat_drop_rate, coefs_drop_rate, reduction=True)
 
         self.feat_drop_rate = feat_drop_rate
 
         
     def call(self, x, training=True):
         # Input x has shape (n_nodes, features)
-        drop_rate = self.feat_drop_rate if training else 0.0
+        feat_dr = self.feat_drop_rate if training else 0.0
 
         for att in self.atts:
-            drop_inp = tf.nn.dropout(x, drop_rate)
-            x = att(drop_inp)
+            drop_inp = tf.nn.dropout(x, feat_dr)
+            x = att(drop_inp, training)
 
-        drop_h = tf.nn.dropout(x, drop_rate)
-        out = self.att_out(drop_h)
+        drop_h = tf.nn.dropout(x, feat_dr)
+        out = self.att_out(drop_h, training)
 
         return tf.squeeze(out)
