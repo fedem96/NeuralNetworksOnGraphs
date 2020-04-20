2352d08738caf859725cf6bf119327c72a8bca89
Niccolò Biondi <niccolobiondi5@gmail.com>
2020-04-20 09:18:47 +0200

    commit last results; add remove outlier in results

--- results.py
+++ results.py
@@ -2,16 +2,32 @@ import argparse
 import numpy as np
 import pandas as pd
 
+# outlier if far from the mean more than n*std
+def reject_outliers(data, n=3):
+    m = np.mean(data)
+    s = np.std(data)
+    return data[np.where(np.abs(data - m) < n * s)]
+
 def compute_statistics(df, seed_value):
 
     statistics = np.zeros((2,2))
 
     opt_values = df[df["data-seed"]==seed_value]["bw_test_accuracy"].values
+    prev_shape = opt_values.shape
+    opt_values = reject_outliers(opt_values)
+    outs_opt = -opt_values.shape[0] + prev_shape[0]
+    print("opt seed outliers", outs_opt)
+
 
     statistics[0][0] = np.mean(opt_values) * 100 # transform to percentages
     statistics[0][1] = np.std(opt_values)  * 100
-    
+     
     net_values = df[df["net-seed"]==seed_value]["bw_test_accuracy"].values
+    prev_shape = net_values.shape
+    net_values = reject_outliers(net_values)
+    outs_net = -net_values.shape[0] + prev_shape[0]
+   
+    print("net seed outliers", outs_net)
 
     statistics[1][0] = np.mean(net_values) * 100
     statistics[1][1] = np.std(net_values)  * 100
@@ -21,7 +37,7 @@ def compute_statistics(df, seed_value):
 def report_statistics(f_path, seed_value, nrows):
     
     # read the first 60 csv lines (last experiments)
-    df = pd.read_csv(f_path, sep =',', usecols=["operation", "data-seed", "net-seed", "dataset", "bw_test_accuracy"]), nrows=nrows)
+    df = pd.read_csv(f_path, sep =',', usecols=["operation", "data-seed", "net-seed", "dataset", "bw_test_accuracy"], nrows=nrows)
     # extract model name from csv
     model_name = df["operation"][0].split('|')[0]
 
